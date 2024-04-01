5e607247513cbad0a6ee6838e821d2bdab32909b
Thomas Neumann <thomasneumann21800@gmail.com>
2024-04-01 11:02:04 -0500

    Added more styles and updated functionality inside modal

--- js/dcf-gallery-image.js
+++ js/dcf-gallery-image.js
@@ -15,7 +15,7 @@ export class DCFGalleryImageTheme {
 
 export class DCFGalleryImage {
   // Set up the button
-  constructor(galleryImages, theme, bodyScrollLock, options = {}) {
+  constructor(galleryImages, theme, bodyScrollLock) {
     if (theme instanceof DCFGalleryImageTheme) {
       this.theme = theme;
     } else {
@@ -69,7 +69,6 @@ export class DCFGalleryImage {
       }
 
       this.addNewGalleryImage(galleryImage);
-      console.dir(galleryImage);
     });
   }
 
@@ -77,33 +76,67 @@ export class DCFGalleryImage {
    * @returns { HTMLDivElement }
    */
   createEmptyModal() {
-    let modalHeading = document.createElement('h2');
-    modalHeading.classList.add('dcf-sr-only');
-    modalHeading.innerHTML = 'Image Gallery';
-
-    let modalCloseButton = document.createElement('button');
-    modalCloseButton.classList.add('dcf-btn-close-modal');
-    modalCloseButton.innerHTML = 'Close';
-
-    let modalHeadDiv = document.createElement('div');
-    modalHeadDiv.classList.add('dcf-modal-header');
-    modalHeadDiv.append(modalHeading);
-    modalHeadDiv.append(modalCloseButton);
-
-    let modalBodyDiv = document.createElement('div');
-    modalBodyDiv.classList.add('dcf-modal-content');
-
-    let modalWrapperDiv = document.createElement('div');
-    modalWrapperDiv.classList.add('dcf-modal-wrapper');
-    modalWrapperDiv.append(modalHeadDiv);
-    modalWrapperDiv.append(modalBodyDiv);
-
     let modalDiv = document.createElement('div');
     modalDiv.classList.add('dcf-modal');
     modalDiv.dataset.galleryImage = 'galleryImage';
     modalDiv.setAttribute('id', this.modalId);
     modalDiv.setAttribute('hidden', 'hidden');
-    modalDiv.append(modalWrapperDiv);
+    modalDiv.innerHTML = `
+    <div class="dcf-modal-wrapper">
+      <div class="dcf-modal-header">
+        <h2 class="dcf-sr-only">Image Gallery</h2>
+        <button class="dcf-btn-close-modal">Close</button>
+      </div>
+      <div class="dcf-modal-content dcf-gallery-image-modal-content">
+        <figure class="dcf-d-flex dcf-flex-col dcf-flex-nowrap dcf-ai-center dcf-jc-center">
+        </figure>
+        <div class="dcf-d-flex dcf-flex-col dcf-flex-nowrap">
+          <div class="dcf-d-flex dcf-flex-row dcf-flex-nowrap dcf-jc-flex-end dcf-gap-1 dcf-mr-3">
+            <button
+              class="
+                dcf-btn
+                dcf-btn-secondary
+                dcf-gallery-image-modal-prev
+                dcf-circle
+                dcf-p-0
+                dcf-h-7
+                dcf-w-7
+                dcf-d-flex
+                dcf-jc-center
+                dcf-ai-center
+              "> < </button>
+              <button
+              class="
+                dcf-btn
+                dcf-btn-secondary
+                dcf-gallery-image-modal-next
+                dcf-circle
+                dcf-p-0
+                dcf-h-7
+                dcf-w-7
+                dcf-d-flex
+                dcf-jc-center
+                dcf-ai-center
+              "> > </button>
+          </div>
+          <ul
+            class="
+              dcf-gallery-image-modal-list
+              dcf-d-flex
+              dcf-flex-row
+              dcf-flex-nowrap
+              dcf-overflow-y-hidden
+              dcf-overflow-x-auto
+              dcf-m-0
+              dcf-p-0
+              dcf-gap-3
+            "
+            role="list"
+          ></ul>
+        </div>
+      </div>
+    </div>
+    `;
 
     return modalDiv;
   }
@@ -117,12 +150,9 @@ export class DCFGalleryImage {
     }
 
     imageElement.classList.add('dcf-btn-toggle-modal');
-    imageElement.classList.add('dcf-btn');
     imageElement.setAttribute('type', 'button');
-    imageElement.setAttribute('disabled', 'disabled');
     imageElement.dataset.ready = 'ready';
     imageElement.dataset.togglesModal = this.modalId;
-    this.modalObj.btnToggleListen(imageElement, this.modalId, imageElement.getAttribute('id'));
 
     this.galleryImageRunningTotal++;
   }
@@ -131,13 +161,119 @@ export class DCFGalleryImage {
    */
   createModalEventListeners() {
     const modalPreOpenEventName = `ModalPreOpenEvent_${this.modalId}`;
+    const modalPostCloseEventName = `ModalCloseEvent_${this.modalId}`;
     document.addEventListener(modalPreOpenEventName, this.openModal.bind(this));
+    document.addEventListener(modalPostCloseEventName, this.closeModal.bind(this));
+
+    document.addEventListener('click', (event) => {
+      const closestGalleryImage = event.target.closest('img.dcf-gallery-img');
+      if (closestGalleryImage === null) {
+        return;
+      }
+
+      if (closestGalleryImage.dataset.ready !== 'ready') {
+        this.addNewGalleryImage(closestGalleryImage);
+      }
+
+      this.modalObj.toggleModal(this.modalId, closestGalleryImage.getAttribute('id'));
+    });
   }
 
   openModal(event) {
-    console.log(event);
+    const modal = document.getElementById(this.modalId);
+    if (modal === null) {
+      throw new Error('Can not find gallery image modal!');
+    }
+
     const allGalleryImages = document.querySelectorAll('img.dcf-gallery-img');
-    const startingImage = event.detail.btn;
-    console.log(allGalleryImages, startingImage);
+    let startingImage = event.detail.btn || null;
+    if (allGalleryImages.length === DCFUtility.magicNumbers('int0')) {
+      throw new Error('Can not find any gallery images!');
+    }
+    if (startingImage === null) {
+      startingImage = allGalleryImages[DCFUtility.magicNumbers('int0')];
+    }
+
+    const list = modal.querySelector('ul');
+    if (list === null) {
+      throw new Error('Can not find gallery image figure!');
+    }
+
+    list.innerHTML = '';
+    allGalleryImages.forEach((singleImage) => {
+      if (singleImage.dataset.ready !== 'ready') {
+        this.addNewGalleryImage(singleImage);
+      }
+
+      let isSelected = false;
+      if (singleImage.getAttribute('id') === startingImage.getAttribute('id')) {
+        this.replaceMainImageForModal(singleImage);
+        isSelected = true;
+      }
+
+      const singleCopiedImage = this.cleanImageForModal(singleImage);
+      singleCopiedImage.classList.add('dcf-ratio-child', 'dcf-obj-fit-cover');
+      list.innerHTML = `
+        ${list.innerHTML}
+        <li class="dcf-ratio dcf-ratio-4x3 dcf-w-10 dcf-rounded dcf-overflow-hidden ${isSelected ? 'dcf-gallery-image-selected' : ''}">
+          ${singleCopiedImage.outerHTML}
+        </li>
+      `;
+    });
+  }
+
+  closeModal() {
+    const modal = document.getElementById(this.modalId);
+    if (modal === null) {
+      throw new Error('Can not find gallery image modal!');
+    }
+
+    const list = modal.querySelector('ul');
+    const figure = modal.querySelector('figure');
+    if (list === null) {
+      throw new Error('Can not find gallery image list!');
+    }
+    if (figure === null) {
+      throw new Error('Can not find gallery image figure!');
+    }
+
+    list.innerHTML = '';
+    figure.innerHTML = '';
+  }
+
+  replaceMainImageForModal(imageElement) {
+    const modal = document.getElementById(this.modalId);
+    if (modal === null) {
+      throw new Error('Can not find gallery image modal!');
+    }
+
+    const figure = modal.querySelector('figure');
+    if (figure === null) {
+      throw new Error('Can not find gallery image figure!');
+    }
+
+    const copiedImageElement = this.cleanImageForModal(imageElement);
+    figure.innerHTML = `
+      ${copiedImageElement.outerHTML}
+      <figcaption class="dcf-figcaption">
+        <div class="dcf-gallery-image-credit">
+          ${copiedImageElement.dataset.credit || ''}
+        </div>
+        <div class="dcf-gallery-image-cutline">
+          ${copiedImageElement.dataset.cutline || ''}
+        </div>
+      </figcaption>
+    `;
+  }
+
+  cleanImageForModal(imageElement) {
+    let copiedImageElement = imageElement.cloneNode();
+    copiedImageElement.classList.remove('dcf-gallery-img');
+    copiedImageElement.classList.remove('dcf-btn-toggle-modal');
+    copiedImageElement.removeAttribute('type');
+    copiedImageElement.removeAttribute('disabled');
+    copiedImageElement.removeAttribute('id');
+
+    return copiedImageElement;
   }
 }
